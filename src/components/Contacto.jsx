607c2656992f1a88abaef976f088cc0288ac59e4
Max Ihnen <166054107+maxdenuevo@users.noreply.github.com>
2025-01-20 12:33:07 -0300

    Update Contacto.jsx
    removing PPI

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -16,11 +16,9 @@ function Contacto() {
                     <p>Si tienes alguna pregunta o deseas más información, no dudes en contactarnos:</p>
                     <div className="mb-3">
                     <h5>• Correo Electrónico:</h5>
-                    <p><a href="mailto:contrato@freelancehub.cl">contrato@freelancehub.cl</a></p>
+                    <p><a href="mailto:contacto@freelancehub.cl">contacto@freelancehub.cl</a></p>
                     <h5>• Números de Contacto:</h5>
                     <p><a href="tel:+56931798920">+56 9 3179 8920</a></p>
-                    <p><a href="tel:+56991536865">+56 9 9153 6865</a></p>
-                    <p><a href="tel:+56994770080">+56 9 9477 0080</a></p>
                     <button onClick={ClickAtras} className="btn mt-2">Atrás</button>
                     </div>
                 </div>
